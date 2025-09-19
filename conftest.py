b32250ff9b9a1d4669892d18fd5be038896c6e37
John Doe <mikee13@ya.ru>
2025-09-19 17:26:46 +0400

    Добавил

--- conftest.py
+++ conftest.py
@@ -1,24 +1,29 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='ru',
+    parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru or en")
 
 @pytest.fixture(scope="function")
-def language(request):
-    browser_language = request.config.getoption("language")
-    language = None
-    if browser_language == 'ru':
-        browser = webdriver.Chrome()
+def browser(request):
+    language = request.config.getoption("language")
+    language_browser = None
+    options = Options()
+    if language == "ru":
+        print("\nstart chrome browser for test..")
         link = "http://selenium1py.pythonanywhere.com/ru/"
-      
-    elif language == 'en':
-        browser = webdriver.Chrome()
+        browser.get(link)
+    elif language == "en":
+        print("\nstart chrome browser for test..")
         link = "http://selenium1py.pythonanywhere.com/en-gb/"
+        browser.get(link)
     else:
-        raise pytest.UsageError("--language should be ru or en")
+        raise pytest.UsageError("--browser_name should en or ru")
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
+from .pages.main_page import MainPage
 import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(language):
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
