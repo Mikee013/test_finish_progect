282180f5bdf3094294df55aacf4e768b7f7fa89e
John Doe <mikee13@ya.ru>
2025-09-24 10:49:18 +0400

    Обновил

--- conftest.py
+++ conftest.py
@@ -2,28 +2,35 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language: ru or en")
+    parser.addoption(
+        "--language", 
+        action="store", 
+        default="en", 
+        help="Choose language: en or ru"
+    )
 
-@pytest.fixture(scope="function")
-def browser(request):
-    language = request.config.getoption("language")
-    language_browser = None
-    options = Options()
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome()  
+    yield driver
+    driver.quit()
+ 
+def test_language_selection(browser, language):
+        
     if language == "ru":
-        print("\nstart chrome browser for test..")
-        link = "http://selenium1py.pythonanywhere.com/ru/"
-        browser.get(link)
+        url == "http://selenium1py.pythonanywhere.com/ru/"
+        time.slip(10)
+        pass
     elif language == "en":
-        print("\nstart chrome browser for test..")
-        link = "http://selenium1py.pythonanywhere.com/en-gb/"
-        browser.get(link)
-    else:
-        raise pytest.UsageError("--browser_name should en or ru")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+        url == "http://selenium1py.pythonanywhere.com/en-gb/"
+        time.slip(10)
+        pass
+
+    # Проверка, что язык выбран правильно
+    assert browser.current_url == f"http://selenium1py.pythonanywhere.com/?lang={language}"
 
 
